9642750f0ee937969f9a7dffaf91bb6d07ef274e
Razorback360 <zezo3601@outlook.com>
2025-01-06 13:01:38 +0300

    chore: migrate prior commit changes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,6 +5,8 @@ import LinkedInProvider from "next-auth/providers/linkedin"
 import AzureAdProvider from "next-auth/providers/azure-ad"
 import GoogleProvider from "next-auth/providers/google"
 import CredentialsProvider from "next-auth/providers/credentials"
+import { compare } from "@app/utils/hash"
+
 // const ORCID_CLIENT_ID = process.env.ORCID_CLIENT_ID
 // const ORCID_CLIENT_SECRET = process.env.ORCID_CLIENT_SECRET
 const LINKEDIN_CLIENT_ID = process.env.LINKEDIN_CLIENT_ID as string
@@ -17,7 +19,6 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string
 
 
 export const authOptions = {
-    // Configure one or more authentication providers
     providers: [
         LinkedInProvider({ clientId: LINKEDIN_CLIENT_ID, clientSecret: LINKEDIN_CLIENT_SECRET }),
         AzureAdProvider({ clientId: AZURE_AD_CLIENT_ID, clientSecret: AZURE_AD_CLIENT_SECRET, tenantId: AZURE_AD_TENANT_ID }),
@@ -26,6 +27,26 @@ export const authOptions = {
             credentials: {
                 email: { label: "Email" },
                 password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials) {
+                if (credentials?.email && credentials?.password) {
+                    const user = await prisma.user.findFirst({
+                        where: {
+                            email: credentials.email
+                        }
+                    })
+                    if (user && user.passHash) {
+                        const result = await compare(credentials.password, user.passHash)
+                        if (result) {
+                            return {
+                                id: user?.id,
+                                username: user?.name,
+                                image: user?.image,
+                                email: user?.email
+                            }
+                        } else return null;
+                    } else return null;
+                } else return null;
             }
         }
         )

53f14b4b5aeff2a58ee2bb215cf5fb015748d282
Razorback360 <zezo3601@outlook.com>
2025-01-09 22:37:14 +0300

    fix: feedback page converted to tailwind

--- src/pages/feedback/index.tsx
+++ src/pages/feedback/index.tsx
@@ -1,55 +1,62 @@
 import Button from '@app/components/Button';
-import './feedback.css';
-
 
 const Feedback = () => {
   return (
-    <div className="feedback-main">
-      <div className="feedback-image">
+    <div className="flex flex-col md:flex-row p-8 justify-center items-center w-full min-h-screen">
+      <div className="w-full md:w-1/2 flex justify-center items-center">
         <img
           src="/feedback.png"
           alt="Feedback Illustration"
-          className="feed-image"
+          className="max-w-full h-auto"
         />
       </div>
-      <div className="feedback-form">
-      <form className='w-full flex flex-col'>
-        <h1 className="feedback-header text-3xl font-bold">Feedback</h1>
-        <p className="feedback-description text-md mt-2">Write your detailed feedback.</p>
-          <div className="name-group">
-            <div className="form-group">
-              <label htmlFor="first-name" className='text-lg font-semibold'>First Name</label>
+      <div className="w-full md:w-1/2 p-4">
+        <form className="w-full flex flex-col">
+          <h1 className="text-3xl font-bold mb-2">Feedback</h1>
+          <p className="text-md mt-2 mb-6">Write your detailed feedback.</p>
+          <div className="flex flex-col md:flex-row w-full gap-4 mb-4">
+            <div className="flex flex-col w-full">
+              <label htmlFor="first-name" className="text-lg font-semibold mb-1">First Name</label>
               <input
                 type="text"
                 id="first-name"
                 placeholder="Placeholder"
                 required
+                className="w-full p-3 bg-gray-100 border border-gray-200 rounded"
               />
             </div>
-            <div className="form-group">
-              <label htmlFor="last-name" className='text-lg font-semibold'>Last Name</label>
+            <div className="flex flex-col w-full">
+              <label htmlFor="last-name" className="text-lg font-semibold mb-1">Last Name</label>
               <input
                 type="text"
                 id="last-name"
                 placeholder="Placeholder"
                 required
+                className="w-full p-3 bg-gray-100 border border-gray-200 rounded"
               />
             </div>
           </div>
-          <div className="form-group">
-            <label htmlFor="email" className='text-lg font-semibold'>Email</label>
-            <input type="email" id="email" placeholder="Placeholder" required />
+          <div className="flex flex-col w-full mb-4">
+            <label htmlFor="email" className="text-lg font-semibold mb-1">Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Placeholder"
+              required
+              className="w-full p-3 bg-gray-100 border border-gray-200 rounded"
+            />
           </div>
-          <div className="form-group ">
-            <label htmlFor="feedback" className='text-lg font-semibold'>Feedback</label>
+          <div className="flex flex-col w-full mb-4">
+            <label htmlFor="feedback" className="text-lg font-semibold mb-1">Feedback</label>
             <textarea
               id="feedback"
               placeholder="Placeholder"
               rows={4}
               required
+              className="w-full p-3 bg-gray-100 border border-gray-200 rounded"
             />
           </div>
-          <Button type="submit" className="bg-blue-500 font-semibold text-white">
+          <Button type="submit" className="bg-primary font-semibold text-white p-3 rounded mt-6">
             Submit
           </Button>
         </form>

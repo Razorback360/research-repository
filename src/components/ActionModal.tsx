89e0d7ae3fc9ba78f6bdf2ca756041161d231d17
Razorback360 <zezo3601@outlook.com>
2025-03-26 23:10:21 +0300

    fix: fix types, fix color

--- src/components/ActionModal.tsx
+++ src/components/ActionModal.tsx
@@ -1,13 +1,13 @@
 import { axiosInstance } from "@app/utils/fetcher";
 import React, { useState } from "react";
 import { HiXMark } from "react-icons/hi2";
-import { Submission } from "@app/components/SubmissionManagement";
+import { Submission } from "@interfaces/index";
 
 interface ActionModalProps {
   isOpen: boolean;
   onClose: () => void;
   type: "deny" | "edit" | null;
-  submission: Submission;
+  submission: Submission | null;
   updateSubmissions: () => void;
 }
 
@@ -82,7 +82,7 @@ function ActionModal({ isOpen, onClose, type, submission, updateSubmissions}: Ac
             </button>
             <button
               type="submit"
-              className="bg-blue-600 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              className="bg-primary py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
             >
               Confirm
             </button>

3b4211376a399d667e00a19c3985af09566e45f7
Razorback360 <82079079+Razorback360@users.noreply.github.com>
2025-06-22 23:04:22 +0300

    fix: reconfigure internationalisation

--- src/pages/i18n.ts
+++ src/pages/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import Backend from 'i18next-http-backend';
+import HttpApi from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 // don't want to use this?
 // have a look at the Quick start guide 
@@ -11,22 +11,26 @@ i18n
   // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
   // learn more: https://github.com/i18next/i18next-http-backend
   // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
-  .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(HttpApi)
   .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: 'en',
     debug: true,
-
+    fallbackLng: 'en',
+    supportedLngs: ['en', 'ar'],
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
+    },
+    load: "languageOnly",
+    preload: ['en', 'ar'],
+    backend: {
+        loadPath: `${process.env.NEXT_PUBLIC_APP_URL || 'http://127.0.0.1:3000'}/locales/{{lng}}/translation.json`,
     }
-  });
+  },
+);
 
 
 export default i18n;

a9df6d56c34007641727551fa651b5a236626a19
Razorback360 <82079079+Razorback360@users.noreply.github.com>
2025-06-22 14:35:41 +0300

    feat: configure i18n

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,24 @@
 import "@app/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import Header from "@app/components/Header"
-import Footer from "@app/components/Footer"
+import Header from "@app/components/Header";
+import Footer from "@app/components/Footer";
+import "@app/pages/i18n";
+import { useTranslation } from "react-i18next";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+  const { i18n } = useTranslation();
   return (
     <SessionProvider session={session}>
-      <div className="flex flex-col h-screen relative min-h-screen">
-      <Header/>
-      <div className="flex-grow">
-      <Component {...pageProps}/>
-      </div>
-      <Footer/>
+      <div className="flex flex-col h-screen relative min-h-screen" dir={i18n.dir()}>
+        <Header />
+        <div className="flex-grow">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
     </SessionProvider>
   );

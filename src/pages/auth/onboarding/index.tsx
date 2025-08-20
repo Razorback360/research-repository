4608515fc80310a8d7fef2b0cac07cb4cc261e85
Razorback360 <razorback@movies4discord.tk>
2025-08-20 21:37:27 +0300

    chore: create onboarding page file

---
+++ src/pages/auth/onboarding/index.tsx
@@ -0,0 +1,15 @@
+import type { FC } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Loader from "@app/components/Loader";
+import { appFetcher } from "@app/utils/fetcher";
+
+const Onboarding: FC = () => {
+
+    return (
+        <div></div>
+    );
+};
+
+export default Onboarding;
